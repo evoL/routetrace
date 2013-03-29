0ecb7e516ca7d7bc8dd6d2c952b0d419d8e17cfc
Rafał Hirsz <rafal@hirsz.co>
2013-03-29 19:03:52 +0100

    Initial Commit

---
+++ routetrace/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  routetrace
+//
+//  Created by Rafał Hirsz on 29.03.2013.
+//  Copyright (c) 2013 Rafał Hirsz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
